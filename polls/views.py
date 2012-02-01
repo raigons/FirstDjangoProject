29ec01fd72a392e4b87bfa04d3259fb6d53c49e9
Ramon Gonçalves <raigons@gmail.com>
2012-02-01 19:08:27 -0200

    first step of django tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from polls.models import Poll
+from django.shortcuts import render_to_response, get_object_or_404
+
+
+def index(request):
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+  	return render_to_response('polls/index.html', {'latest_poll_list':latest_poll_list})
+
+def detail(request, poll_id):
+	poll = get_object_or_404(Poll, pk=poll_id)
+	return render_to_response('polls/detail.html', {'poll': poll})
+
+def results(request, poll_id):
+	return HttpResponse("You are looking at the results of poll %s" % poll_id)
+
+def vote(request, poll_id):
+	return HttpResponse("You're voting on poll %s" % poll_id)
